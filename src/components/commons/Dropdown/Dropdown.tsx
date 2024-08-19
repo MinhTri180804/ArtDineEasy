633f95c8ab420f9d866006c4db0f47a98f1b91e8
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-08-19 14:47:07 +0700

    feat(component): add dropdown component

--- src/components/commons/Dropdown/Dropdown.tsx
+++ src/components/commons/Dropdown/Dropdown.tsx
@@ -1,26 +1,27 @@
 import React, { ComponentType, useId } from 'react';
+import './styles.scss';
 
 interface IDropdown {
   IconLeft?: ComponentType<React.SVGProps<SVGSVGElement>>;
-  ImageRight?: ComponentType<React.SVGProps<SVGSVGElement>>;
+  imageRight?: string;
   title: string;
   classNames?: string;
 }
 
-const Dropdown = ({ IconLeft, ImageRight, title, classNames }: IDropdown) => {
+const Dropdown = ({ IconLeft, imageRight, title, classNames }: IDropdown) => {
   const id = useId();
 
   // condition render for dropdown have both is icon left and image right
-  if (IconLeft && ImageRight) {
+  if (IconLeft && imageRight) {
     return (
       <div
         className={`dropdown__component ${id} icon__left image__right ${
-          classNames && classNames
+          classNames ?? ''
         }`}
       >
         <div className="dropdown__component-title">
           <div className="dropdown__component-title--icon">
-            <ImageRight />
+            <img src={imageRight} alt="" />
           </div>
 
           <div className="dropdown__component-title--value">Việt Nam</div>
@@ -37,9 +38,7 @@ const Dropdown = ({ IconLeft, ImageRight, title, classNames }: IDropdown) => {
   if (IconLeft) {
     return (
       <div
-        className={`dropdown__component ${id} icon__left ${
-          classNames && classNames
-        }`}
+        className={`dropdown__component ${id} icon__left ${classNames ?? ''}`}
       >
         <div className="dropdown__component-title">
           <div className="dropdown__component-title--value">Việt Nam</div>
@@ -53,17 +52,28 @@ const Dropdown = ({ IconLeft, ImageRight, title, classNames }: IDropdown) => {
   }
 
   // condition render for dropdown has only image right
-  return (
-    <div
-      className={`dropdown__component ${id}  image__right ${
-        classNames && classNames
-      }`}
-    >
-      <div className="dropdown__component-title">
-        <div className="dropdown__component-title--icon">
-          {ImageRight && <ImageRight />}
-        </div>
+  if (imageRight) {
+    return (
+      <div
+        className={`dropdown__component ${id}  image__right ${
+          classNames || ''
+        }`}
+      >
+        <div className="dropdown__component-title">
+          <div className="dropdown__component-title--icon">
+            <img src={imageRight} alt="" />
+          </div>
 
+          <div className="dropdown__component-title--value">{title}</div>
+        </div>
+      </div>
+    );
+  }
+
+  // condition render for dropdown default, don't have icon left or image right
+  return (
+    <div className={`dropdown__component ${id}   ${classNames ?? ''}`}>
+      <div className="dropdown__component-title">
         <div className="dropdown__component-title--value">{title}</div>
       </div>
     </div>
--- src/pages/GlobalCustomer/Home/HomePage.tsx
+++ src/pages/GlobalCustomer/Home/HomePage.tsx
@@ -1,12 +1,14 @@
 import ButtonComponent from '../../../components/commons/Button';
 import Dropdown from '../../../components/commons/Dropdown';
+import flagVN from '../../../assets/images/FlagVN.svg';
+import { ArrowDownIcon } from '../../../assets/icons';
 
 const HomePage = () => {
   return (
     <div className="container__page">
       <div className="container__page-title">This is Home page</div>
       <ButtonComponent />
-      <Dropdown title="testing" />
+      <Dropdown title="testing" imageRight={flagVN} IconLeft={ArrowDownIcon} />
     </div>
   );
 };
