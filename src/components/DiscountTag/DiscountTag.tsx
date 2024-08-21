f5a283848e957202c98c7cd862080238b0056351
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-08-21 10:45:13 +0700

    refactor(component): refactor discount tag component

--- src/components/DiscountTag/DiscountTag.tsx
+++ src/components/DiscountTag/DiscountTag.tsx
@@ -3,14 +3,28 @@ import { TagIcon } from '../../assets/icons';
 
 interface IDiscountTagComponentProps {
   // here props type
-  textContent: string;
+  textContent?: string;
   size?: 'small' | 'medium' | 'large';
+  moreValue?: number;
 }
 
 const DiscountTagComponent = ({
-  textContent,
+  textContent = 'discount text default',
   size = 'small',
+  moreValue,
 }: IDiscountTagComponentProps) => {
+  if (moreValue) {
+    return (
+      <div className="discount__tag-component">
+        <div className="icon">
+          <TagIcon />
+        </div>
+
+        <div className={`content more-value ${size}`}>+ {moreValue}</div>
+      </div>
+    );
+  }
+
   return (
     <div className="discount__tag-component">
       <div className="icon">
