cf07f9e9864de8a0789d6284e7e925273463cac1
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-08-21 10:31:09 +0700

    feat(component): add discountTag component

--- src/components/DiscountTag/DiscountTag.tsx
+++ src/components/DiscountTag/DiscountTag.tsx
@@ -1,13 +1,23 @@
 import './styless.scss';
+import { TagIcon } from '../../assets/icons';
 
 interface IDiscountTagComponentProps {
   // here props type
+  textContent: string;
+  size?: 'small' | 'medium' | 'large';
 }
 
-const DiscountTagComponent = ({}: IDiscountTagComponentProps) => {
+const DiscountTagComponent = ({
+  textContent,
+  size = 'small',
+}: IDiscountTagComponentProps) => {
   return (
-    <div>
-      <div>This is discount tag</div>
+    <div className="discount__tag-component">
+      <div className="icon">
+        <TagIcon />
+      </div>
+
+      <div className={`content ${size}`}>{textContent}</div>
     </div>
   );
 };
