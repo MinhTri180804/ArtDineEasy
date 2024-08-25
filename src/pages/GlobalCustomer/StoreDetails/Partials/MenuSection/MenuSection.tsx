408d6c70e5fcc75da83631513a73963186dd56b8
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-08-25 15:02:37 +0700

    feat(component): init menuSection of store details page

---
+++ src/pages/GlobalCustomer/StoreDetails/Partials/MenuSection/MenuSection.tsx
@@ -0,0 +1,15 @@
+const MenuSection = () => {
+  return (
+    <div className="menu__section">
+        <div className="menu__section-nav">
+            {/* TODO: Implement nav list component of menu in here */}
+        </div>
+
+        <div className="menu__section-content">
+            {/* TODO: Implement section of menu component in here */}
+        </div>
+    </div>
+  );
+};
+
+export { MenuSection };
